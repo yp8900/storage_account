7fae5a8fc8636aae8118dc8b057114f0bfeb1f6c
Your Name <your-email@example.com>
2025-03-28 11:48:42 +0530

    storage module commit

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -2,3 +2,11 @@ output "resource_group_id" {
   description = "ID of the Resource Group"
   value       = azurerm_resource_group.this.id
 }
+
+
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+output "resource_group_location" {
+  value = azurerm_resource_group.this.location
+}
